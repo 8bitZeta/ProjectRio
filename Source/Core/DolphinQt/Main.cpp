330c4210b21935e5687994b5d3c61e648063d69f
LittleCoaks <LittleCoaks@users.noreply.github.com>
2024-08-05 02:41:01 -0400

    Default Font update
    Idk i like it better and it'll fit the UI needs later

--- Source/Core/DolphinQt/Main.cpp
+++ Source/Core/DolphinQt/Main.cpp
@@ -181,6 +181,8 @@ int main(int argc, char* argv[])
   QObject::connect(QAbstractEventDispatcher::instance(), &QAbstractEventDispatcher::aboutToBlock,
                    &app, &Core::HostDispatchJobs);
 
+  app.setFont(QFont(QStringLiteral("Helvetica"), 14));
+
   std::optional<std::string> save_state_path;
   if (options.is_set("save_state"))
   {

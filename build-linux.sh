b9ace083d0cab2266f75f223c4dcda798f2f74c6
8bitZeta <devin94@protonmail.com>
2026-05-22 22:29:35 -0500

    Utilize `ccache` to improve build speeds

--- build-linux.sh
+++ build-linux.sh
@@ -3,11 +3,13 @@
 
 CMAKE_FLAGS='-DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DLINUX_LOCAL_DEV=true -DCMAKE_BUILD_WITH_INSTALL_RPATH=true -DCMAKE_BUILD_TYPE=Release -GNinja'
 
+# Use ccache if available (speeds up incremental CI builds significantly)
+if command -v ccache &> /dev/null; then
+    CMAKE_FLAGS+=' -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache'
+fi
+
 DATA_SYS_PATH="./Data/Sys/"
 BINARY_PATH="./build/Binaries/"
-LOCAL_SHARE="$HOME/.local/share"
-DATA_ICON_PATH="./Data/ProjectRio.png"
-DATA_DESKTOP_PATH="./Data/ProjectRio.desktop"
 
 # Move into the build directory, run CMake, and compile the project
 mkdir -p build
